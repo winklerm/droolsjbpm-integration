53c367c1102e8a9d6e5643748f0d46ff0c021116
Marek Winkler <mwinkler@redhat.com>
2016-09-01 17:29:14 +0200

    kie-aries-blueprint: add tests for kie:import - KieScanner - draft

--- drools-osgi/drools-karaf-itests/src/test/java/org/drools/karaf/itest/blueprint/KieBlueprintImportScannerIntegrationTest.java
+++ drools-osgi/drools-karaf-itests/src/test/java/org/drools/karaf/itest/blueprint/KieBlueprintImportScannerIntegrationTest.java
@@ -17,37 +17,44 @@
 package org.drools.karaf.itest.blueprint;
 
 import org.drools.karaf.itest.AbstractKarafIntegrationTest;
-import org.drools.karaf.itest.blueprint.domain.Customer;
-import org.drools.karaf.itest.blueprint.domain.Drink;
-import org.drools.karaf.itest.blueprint.domain.Order;
+import org.drools.karaf.itest.util.KieScannerTestUtils;
+import org.drools.karaf.itest.util.TimerUtils;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kie.api.KieBase;
+import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
+import org.kie.api.builder.ReleaseId;
 import org.kie.api.runtime.KieSession;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.osgi.framework.Constants;
 
 import javax.inject.Inject;
 
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.streamBundle;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
 import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
 
 @RunWith(PaxExam.class)
-@ExamReactorStrategy(PerMethod.class)
-public class KieBlueprintImportIntegrationTest extends AbstractKarafIntegrationTest {
+@ExamReactorStrategy(PerClass.class)
+public class KieBlueprintImportScannerIntegrationTest extends AbstractKarafIntegrationTest {
 
-    private static final String BLUEPRINT_XML_LOCATION = "/org/drools/karaf/itest/blueprint/kie-scanner-import-blueprint.xml";
+    private static final String BLUEPRINT_XML_LOCATION = "/org/drools/karaf/itest/blueprint/kie-import-scanner-blueprint.xml";
+    private static final ReleaseId RELEASE_ID = KieServices.Factory.get().newReleaseId("org.drools.karaf.itest", "import-scanner-test-jar", "1.0-SNAPSHOT");
 
-    @Inject
-    KieSession kieSession;
+    private KieScannerTestUtils kieScannerTestUtils = new KieScannerTestUtils();
 
     @Inject
     KieBase kieBase;
@@ -55,39 +62,45 @@ public class KieBlueprintImportIntegrationTest extends AbstractKarafIntegrationT
     @Inject
     KieScanner kieScanner;
 
+    @Before
+    public void setUp() {
+        kieScannerTestUtils.setUp();
+        kieScannerTestUtils.createAndInstallKJar(RELEASE_ID, "rule 1");
+    }
+
+    @After
+    public void tearDown() {
+        kieScannerTestUtils.tearDown();
+    }
+
     @Test
-    public void kieElementsExistTest() {
-        Assert.assertNotNull(kieSession);
+    public void testKieBaseUpdatedByScanNow() {
         Assert.assertNotNull(kieBase);
-        Assert.assertNotNull(kieScanner);
+
+        KieSession kieSession = kieBase.newKieSession();
+        kieScannerTestUtils.checkKSession(true, kieSession, "rule 1");
+
+        // update kJar
+        kieScannerTestUtils.createAndInstallKJar(RELEASE_ID, "rule 2");
+        kieScanner.scanNow();
+
+        kieSession = kieBase.newKieSession();
+        kieScannerTestUtils.checkKSession(true, kieSession, "rule 2");
     }
 
     @Test
-    public void kieSessionOldPersonTest() {
-        Assert.assertNotNull(kieSession);
+    public void testKieBaseUpdatedByTimer() throws InterruptedException {
+        Assert.assertNotNull(kieBase);
 
-        Drink drink = new Drink("whiskey", true);
-        Customer customer = new Customer("Customer", 40);
-        Order order = new Order(customer, drink);
+        KieSession kieSession = kieBase.newKieSession();
+        kieScannerTestUtils.checkKSession(true, kieSession, "rule 1");
 
-        kieSession.insert(order);
-        kieSession.fireAllRules();
+        // update kJar
+        kieScannerTestUtils.createAndInstallKJar(RELEASE_ID, "rule 2");
+        TimerUtils.sleepMillis(2000);
 
-        Assert.assertTrue(order.isApproved());
-    }
-
-    @Test
-    public void kieSessionYoungPersonTest() {
-        Assert.assertNotNull(kieSession);
-
-        Drink drink = new Drink("whiskey", true);
-        Customer customer = new Customer("Customer", 14);
-        Order order = new Order(customer, drink);
-
-        kieSession.insert(order);
-        kieSession.fireAllRules();
-
-        Assert.assertFalse(order.isApproved());
+        kieSession = kieBase.newKieSession();
+        kieScannerTestUtils.checkKSession(true, kieSession, "rule 2");
     }
 
     @Configuration
@@ -110,12 +123,11 @@ public class KieBlueprintImportIntegrationTest extends AbstractKarafIntegrationT
                 // Load Kie-Aries-Blueprint
                 loadKieFeatures("drools-module", "kie-ci", "kie-aries-blueprint"),
 
-                // wrap and install junit bundle - the DRL imports a class from it
-                // (simulates for instance a bundle with domain classes used in rules)
-                wrappedBundle(mavenBundle().groupId("junit").artifactId("junit").versionAsInProject()),
-
                 // Load domain model classes
                 wrappedBundle(mavenBundle().groupId("org.drools").artifactId("drools-karaf-itests-domain-model").versionAsInProject()),
+
+                // utility classes from Drools-core test jar
+                //wrappedBundle(mavenBundle().groupId("org.drools").artifactId("drools-core").versionAsInProject().type("test-jar")),
 
                 // Create a bundle with META-INF/spring/kie-beans.xml - this should be processed automatically by Spring
                 streamBundle(bundle()
@@ -130,8 +142,6 @@ public class KieBlueprintImportIntegrationTest extends AbstractKarafIntegrationT
                                 "org.kie.api," +
                                 "org.kie.api.runtime," +
                                 "org.kie.api.builder," +
-                                // junit is acting as a dependency for the rule
-                                "org.junit," +
                                 "*")
                         .set(Constants.EXPORT_PACKAGE, "org.drools.karaf.itest.blueprint.domain")
                         .set(Constants.BUNDLE_SYMBOLICNAME, "Test-Blueprint-Bundle")
